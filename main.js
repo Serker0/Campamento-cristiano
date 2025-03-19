c05aa8695f64afa2a3b8540dbe06386b81b2605a
Serker0 <117919752+Serker0@users.noreply.github.com>
2025-03-19 18:17:39 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 function abrirMenu(){
     document.getElementById("leftMenu").style.width = "150px";
+    document.getElementById("leftMenu").style.height= "375px" ;
 }
 function cerrarMenu(){
     document.getElementById("leftMenu").style.width = "0";
