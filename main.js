c2c9717385db915f68912c4f19f784820ee094f2
Serker0 <117919752+Serker0@users.noreply.github.com>
2025-03-30 22:44:41 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 function abrirMenu(){
     document.getElementById("leftMenu").style.width = "150px";
-    document.getElementById("leftMenu").style.height= "300px" ;
+    document.getElementById("leftMenu").style.height= "325px" ;
 }
 function cerrarMenu(){
     document.getElementById("leftMenu").style.width = "0";
